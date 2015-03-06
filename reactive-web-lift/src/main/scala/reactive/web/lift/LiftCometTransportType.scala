5d85f4e2ce9c25e68c00fd65adf9d4bddd5240c8
lvitaly <lvitalii@gmail.com>
2015-03-06 14:56:10 +0200

    comet creation timeout were increased up to 30 seconds.

--- reactive-web-lift/src/main/scala/reactive/web/lift/LiftCometTransportType.scala
+++ reactive-web-lift/src/main/scala/reactive/web/lift/LiftCometTransportType.scala
@@ -2,6 +2,9 @@ package reactive
 package web
 package lift
 
+import java.util.concurrent.TimeUnit
+
+import scala.concurrent.duration.Duration
 import scala.xml.{ NodeSeq, Null, UnprefixedAttribute }
 import net.liftweb.common._
 import net.liftweb.util.Helpers._
@@ -10,13 +13,18 @@ import net.liftweb.http.js.{ JsCmd, JsCmds }
 
 import reactive.logging.HasLogger
 
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object LiftCometTransportType {
+  private val contType = "reactive.web.ReactionsComet"
+
   private val _overrideCometHack = new scala.util.DynamicVariable(Option.empty[LiftCometTransportType#PageComet])
   private var initted = false
 
-  private def overrideCometHack[A](comet: LiftCometTransportType#PageComet)(f: =>A): A =
-    if(!initted)
-      sys.error("LiftCometSupport was not initialized! You must call LiftCometSupport.init() in boot in order to use LiftCometTransportType.")
+  private def overrideCometHack[A](comet: LiftCometTransportType#PageComet)(f: => A): A =
+    if (!initted)
+      sys.error("LiftCometTransport was not initialized! You must call LiftCometTransportType.init() in boot in order to use LiftCometTransport.")
     else
       _overrideCometHack.withValue(Some(comet))(f)
 
@@ -25,20 +33,27 @@ object LiftCometTransportType {
    */
   def init(): Unit = {
     LiftRules.cometCreation.append {
-      case CometCreationInfo(t, name, defaultXml, attributes, session) if _overrideCometHack.value.isDefined =>
-      val comet = _overrideCometHack.value.get
-      comet.initCometActor(session, Full(t), name, defaultXml, attributes)
-      comet
+      case CometCreationInfo(t, name, defaultXml, attributes, session)
+        if _overrideCometHack.value.isDefined && t == contType =>
+        val comet = _overrideCometHack.value.get
+        comet.initCometActor(session, Full(t), name, defaultXml, attributes)
+        comet
     }
     initted = true
   }
 }
 
 class LiftCometTransportType(page: Page) extends TransportType with HasLogger {
+  // Promise that comet actor will be initialized.
+  val initPromise = Promise[Box[String]]()
+  val initFuture = initPromise.future
+
   class PageComet extends CometActor {
     // Make initCometActor accessible
-    override protected[web] def initCometActor(s: LiftSession, t: Box[String], n: Box[String], x: NodeSeq, a: Map[String, String]) =
+    override protected[web] def initCometActor(s: LiftSession, t: Box[String], n: Box[String], x: NodeSeq, a: Map[String, String]): Unit = {
       super.initCometActor(s, t, n, x, a)
+      initPromise success t
+    }
 
     override def lifespan = Full(60.seconds)
 
@@ -49,6 +64,7 @@ class LiftCometTransportType(page: Page) extends TransportType with HasLogger {
       case js: JsCmd => partialUpdate(js)
     }
   }
+
   val comet = new PageComet
 
   object cometTransport extends Transport {
@@ -59,21 +75,29 @@ class LiftCometTransportType(page: Page) extends TransportType with HasLogger {
 
   LiftCometTransportType.overrideCometHack(comet) {
     // This is how we install our own comet actors in Lift
-    S.withAttrs(new UnprefixedAttribute("type", "reactive.web.ReactionsComet", new UnprefixedAttribute("name", page.id, Null))) {
+    S.withAttrs(new UnprefixedAttribute("type", LiftCometTransportType.contType, new UnprefixedAttribute("name", page.id, Null))) {
       net.liftweb.builtin.snippet.Comet.render(NodeSeq.Empty)
     }
   }
 
-  comet !? (comet.cometRenderTimeout, AskRender) foreach {
-    case AnswerRender(_, _, when, _) =>
-      page.queue(s"""var lift_toWatch = lift_toWatch || {}; lift_toWatch['${comet.uniqueId}'] = $when;""")
+  // Ask comet actor for render after his initialization for avoid NPE
+  val askRender = Future {
+    initFuture onSuccess {
+      case Full(_) =>
+        comet !? (comet.cometRenderTimeout, AskRender) foreach {
+          case AnswerRender(_, _, when, _) =>
+            page.queue(s"var lift_toWatch = lift_toWatch || {}; lift_toWatch['${comet.uniqueId}'] = $when;")
+        }
+    }
   }
+  // Waiting for comet actor answer for render lift_toWatch
+  Await.result(askRender, Duration(30, TimeUnit.SECONDS))
 
   override def render = super.render ++ S.session.map{ session =>
     // TODO ensure that it's not already rendered
     val cometSrc =
       List(S.contextPath, LiftRules.cometPath, urlEncode(session.uniqueId), LiftRules.cometScriptName()) mkString "/"
-    <script src={ S.encodeURL(cometSrc) } type="text/javascript"/>
+      <script src={ S.encodeURL(cometSrc) } type="text/javascript"/>
   }.openOr{ logger.warn("Rendering "+this+" outside of session"); NodeSeq.Empty }
 
   linkTransport(cometTransport)
